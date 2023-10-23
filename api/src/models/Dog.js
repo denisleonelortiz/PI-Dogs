0acf06b59d6f0fd8d1290c49ba6c1b8ad252e7f6
Leonel Ortiz <denisleonelortiz@gmail.com>
2023-10-23 19:49:04 -0300

    First commit

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,11 +2,12 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('dog', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+    console.log("hola")
 };
