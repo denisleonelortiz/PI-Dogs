57e1014bfdeb1b9716676bcc541f29ab93592b6d
denisleonelortiz <denisleonelortiz@gmail.com>
2024-09-19 22:30:41 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { chargeTemperamentsDb } = require('./src/utils/index.js');
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
     server.listen(3001, async() => {
         await chargeTemperamentsDb()
-        console.log('%s listening at 3001'); // eslint-disable-line no-console
+        console.log(`server listening at ${port}`); // eslint-disable-line no-console
     });
 });
--- client/src/api/apiInstance.js
+++ client/src/api/apiInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const dogIns = axios.create({
-  baseURL: "https://pi-dogsapi-production.up.railway.app/",
+  baseURL: "https://pi-dogsapi-production.up.railway.app",
   //baseURL: "http://localhost:3001",
 });
